88d04d9a7eac15195593ab5a6bf36b51adb85a06
Aotokitsuruya <contact@aotoki.me>
2024-04-02 16:04:28 +0800

    Refactor create font loader script

--- scripts/create_font_loader.rb
+++ scripts/create_font_loader.rb
@@ -3,31 +3,65 @@
 require 'find'
 require 'pathname'
 
-SUPPORT_CHARACTERS = {}
-File.readlines('res/data/all_griph_HanWangMingHeavy.txt').each do |line|
-  line.scan(/./m) { |ch| SUPPORT_CHARACTERS[ch] = 'OK' }
+EXPORT_PATH = 'tmp/export'
+
+ACTIONSCRIPT_CONSTANT_PATH = "#{EXPORT_PATH}/constants.txt"
+ACTIONSCRIPT_END = /;/.freeze
+EXCLUDE_LOCALE = /LOCALE_EN|LOCALE_JP|LOCALE_SCN|LOCALE_KR|LOCALE_FR|LOCALE_ID|LOCALE_TH/.freeze
+STRING_QUOTE = /"(.*)"|'(.*)'/.freeze
+
+SUPPORT_CHARACTERS = 'res/data/all_griph_HanWangMingHeavy.txt'
+
+support_characters = {}
+File.readlines(SUPPORT_CHARACTERS).each do |line|
+  line.scan(/./m) { |ch| support_characters[ch] = true }
 end
 
+system('grep -C2 -r  "\".*\"" src/ |' \
+       'grep -v "tmp" |' \
+       'grep -v "#" |' \
+       'grep -v -e "writeLog" -e "TAGS" >' +
+       ACTIONSCRIPT_CONSTANT_PATH)
+
+constants = []
+skip_next = false
+File.readlines(ACTIONSCRIPT_CONSTANT_PATH).each do |line|
+  encoded_line = line.force_encoding('UTF-8')
+  if encoded_line.match?(EXCLUDE_LOCALE)
+    skip_next = true
+    next
+  end
+
+  if skip_next
+    skip_next = false if encoded_line.match?(ACTIONSCRIPT_END)
+    next
+  end
+
+  string = encoded_line.match(STRING_QUOTE)
+  constants << string[1] if string
+end
+File.write(ACTIONSCRIPT_CONSTANT_PATH, constants.join("\n"))
+
 # 文字をカウント
-GAME_CHARACTERS = {}
-if File.directory?('./data/export')
-  Find.find('./data/export') do |f|
+game_characters = {}
+if File.directory?(EXPORT_PATH)
+  Find.find(EXPORT_PATH) do |f|
     next if File.directory?(f)
 
     Find.prune if f.split('/').size > 4
 
     File.readlines(f).each do |line|
-      line.scan(/./m) { |ch| GAME_CHARACTERS[ch] = format 'U+%04X', ch.unpack1('U*') }
+      line.scan(/./m) { |ch| game_characters[ch] = format 'U+%04X', ch.unpack1('U*') }
     end
   end
 end
 
 error_count = 0
 
-CHECK_TABLE = GAME_CHARACTERS.dup
-CHECK_TABLE.update(SUPPORT_CHARACTERS)
-CHECK_TABLE.each do |char, unicode|
-  next if unicode == 'OK'
+CHECK_TABLE = game_characters.dup
+CHECK_TABLE.update(support_characters)
+CHECK_TABLE.each do |char, is_unicode|
+  next if is_unicode
 
   puts "This griph undefind #{char},#{unicode}"
   error_count += 1
@@ -37,11 +71,11 @@ puts "Unsupported Characters: #{error_count}."
 file = Pathname.new('src/FontLoader.as')
 file.open('w') do |f|
   f.puts DATA.read
-             .gsub('__font_UTF_NO__', ", unicodeRange='#{GAME_CHARACTERS.values.sort!.join(',')}'")
+             .gsub('__font_UTF_NO__', ", unicodeRange='#{game_characters.values.sort!.join(',')}'")
              .gsub('__FONT_H__', 'wt004.ttf')
              .gsub('__FONT_R__', 'cwming.ttf')
 end
-puts "#{GAME_CHARACTERS.size} 文字を登録しました"
+puts "#{game_characters.size} 文字を登録しました"
 
 __END__
 [Embed(source='../res/data/__FONT_H__', fontName='minchoB'__font_UTF_NO__)]
@@ -50,5 +84,3 @@ private static const Font:Class;
 private static const Font2:Class;
 [Embed(source='../res/data/nbr.ttf', fontName='bradley')]
 private static const Font3:Class;
-
-
